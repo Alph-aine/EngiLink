f60dc4acbfdae64b6df8a0c82677a18b2a5167b3
Abdulbasit <aabdulrafiu721@gmail.com>
2023-12-16 00:07:05 +0100

    Implemented all equivalent endpoints for employer

--- backend/routes/empAuth.js
+++ backend/routes/empAuth.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import * as authController from '../controllers/empAuthcontroller.js';
+import { isAuthenticated } from '../middlewares/auth.js';
 
 const router = express.Router();
 
 const {
   registerEmployer,
-  loginEmployer
+  loginEmployer,
+  getMe,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
+  getEmployerById,
+  getEmployerByCompanyName,
+  deleteEmployerAccount
 } = authController;
 
 // Register employer
@@ -14,4 +22,23 @@ router.route('/employer/register').post(registerEmployer);
 // login employer
 router.route('/employer/login').post(loginEmployer);
 
+// get currently logged in employer profile
+router.route('/employer/me').get(isAuthenticated, getMe);
+
+// forgot password
+router.route('/employer/password/forgot').post(forgotPassword);
+
+// reset password, works with forgot password
+router.route('/employer/password/reset/:token').put(resetPassword);
+
+// update logged in employer password
+router.route('/employer/password/update').put(isAuthenticated, updatePassword);
+
+// get employer by id
+router.route('/employer/id/:id').get(isAuthenticated, getEmployerById);
+// delete employer account
+router.route('/employer/id/:id').delete(isAuthenticated, deleteEmployerAccount);
+
+// get employer by companyName
+router.route('/employer/company/:companyName').get(isAuthenticated, getEmployerByCompanyName);
 export default router;
