b4d79062aa7f474119b2329e59e485db87e5d4fa
Abdulbasit <aabdulrafiu721@gmail.com>
2023-12-15 22:37:15 +0100

    Implemented endpoint to update password when logged in

--- backend/controllers/engrAuthController.js
+++ backend/controllers/engrAuthController.js
@@ -131,3 +131,25 @@ export const resetPassword = asyncErrors(async (req, res, next) => {
 
   sendToken(engineer, 200, res);
 });
+
+// Update or change password of currently logged in user.
+export const updatePassword = asyncErrors(async (req, res, next) => {
+  const engineer = await Engineer.findById(req.user.id).select('+password');
+
+  // Check if old password is correct.
+  const isPasswordValid = await engineer.validatePassword(req.body.oldPassword);
+  if (!isPasswordValid) {
+    return next(new ErrorHandler('Incorrect old password', 400));
+  }
+
+  // Check if new password is same as old password.
+  if (req.body.newPassword === req.body.oldPassword) {
+    return next(new ErrorHandler('New password is same as old password', 400));
+  }
+
+  // Set new password.
+  engineer.password = req.body.newPassword;
+  await engineer.save();
+
+  sendToken(engineer, 200, res);
+});
--- backend/routes/engrAuth.js
+++ backend/routes/engrAuth.js
@@ -9,7 +9,8 @@ const {
   loginEngineer,
   getMe,
   forgotPassword,
-  resetPassword
+  resetPassword,
+  updatePassword
 } = authController;
 
 // Register engineer
@@ -18,7 +19,7 @@ router.route('/engineer/register').post(registerEngineer);
 // login engineer
 router.route('/engineer/login').post(loginEngineer);
 
-// get current logged in user
+// get current logged in engineer profile
 router.route('/engineer/me').get(isAuthenticated, getMe);
 
 // forgot password
@@ -26,5 +27,8 @@ router.route('/engineer/password/forgot').post(forgotPassword);
 
 // reset password
 router.route('/engineer/password/reset/:token').put(resetPassword);
+
+// update logged in engineer password
+router.route('/engineer/password/update').put(isAuthenticated, updatePassword);
 
 export default router;
