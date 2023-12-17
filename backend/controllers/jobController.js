4d9e113827c8509a9fe573f240fa77f2acc37c78
Aloh Michael <mykelaloh@gmail.com>
2023-12-17 11:18:04 -0800

    job controller

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -77,3 +77,100 @@ export const deleteJob = asyncErrors(async (req, res, next) => {
 	    next(new ErrorHandler(error.message, 400));
 	}
     });
+})
+
+// get job controller
+export const  getJobs = asyncErrors(async (req, res, next) => {
+    isAuthenticated(req, res, async (req, res) => {
+	try {
+	    // gets a single job if jobId param is present
+	    const jobId = req.params.jobId;
+	    if (jobId) {
+		const job = Job.findById(jobId);
+		if (!job) {
+		    re.status(404).send('Job not found');
+		}
+		res.json(job);
+	    } else {
+		// gets all jobs if jobid param is missing
+		const jobs = await Job.find();
+		res.json(jobs);
+	    }
+	} catch (error) {
+	    next(new ErrorHandler(error.message, 500));
+	}
+    });
+});
+
+
+// To serach for jobs using sime filters or query
+export const searchJobs = asyncErrors(async (req, res, next) => {
+    isAuthenticated(req, res, async (req, res) => {
+	try {
+	    const {
+		title,
+		skillsRequired,
+		experienceLevel,
+		employmentType,
+		minSalary,
+		maxSalary,
+		location,
+		employer
+	    } = req.query;
+/	    // A filter object to store the search queries of the user
+	    const filter = {};
+	    if (title) filter.title = new RegExp(title, 'i');
+	    if skillsRequired filter.skillsRequired = new RegExp(skillsRequired, 'i');
+	    if experienceLevel filter.experienceLevel = experienceLevel;
+	    if employmentType filter.employmentType = employmentType;
+	    if minSalary filter.minSalary = { $gte: parseInt(minSalary) };
+	    if maxSalary filter.maxSalary = { $lte: parseInt(maxSalary) };
+	    if location filter.location = new RegExp(location, 'i');
+	    if employer filter.employer = new RegExp(employer, 'i');
+
+	    const jobs = await Job.find(filter);
+	    res.json(jobs);
+	} catch(error) {
+	    next(new ErrorHandler(error.message, 500));
+	}
+    });
+});
+
+// update job controller
+export const updateJob = asyncErrors(async (req, res, next) => {
+    isAuthenticated(req, res, async (req, res) => {
+	try {
+	    const jobId = req.params.jobId;
+	    const job = await Job.findById(jobId);
+	    if (!job) {
+		res.status(404).send('Job not found');
+	    }
+	    if (req.user.role === 'employer' && job.employer.toString() === req.user.userId) {
+		const {
+		    title,
+		    skillsRequired,
+		    experienceLevel,
+		    employmentType,
+		    minSalary,
+		    maxSalary,
+		    location,
+		    deadline
+		} = req.body;
+		job.title = title || job.title;
+		job.skillsRequired = skillsRequired || job.skillsRequired;
+		job.experienceLevel = experienceLevel || job.experienceLevel;
+		job.employmentType = employmentType || job.employmentType;
+		job.minSalary = minSalary || job.minSalary;
+		job.maxSalary = maxSalary || job.maxSalary;
+		job.location = location || job.location;
+		job.deadline = deadline || job.deadline;
+		await job.save();
+		res.status(200).json(job);
+	    } else {
+		res.status(403).send('Permission denied to update the job');
+	    }
+	} catch (error) {
+	    next(new ErrorHandler(error.message, 500));
+	}
+    });
+});
