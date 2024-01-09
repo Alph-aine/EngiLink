d57cf68bb06fd03f591460a1a48174432279c1d3
Mmesoma Saint <mmesomasaint@gmail.com>
2024-01-09 09:04:59 +0100

    Feat: Create btn component

---
+++ frontend/src/components/button.jsx
@@ -0,0 +1,33 @@
+import Text from './text'
+
+export default function Button({
+  children,
+  onClick,
+  cx,
+  type = 'button',
+  disabled = false,
+  outline = false,
+  textBlack = false,
+}) {
+  const bg = cx ? cx : 'bg-primary'
+  const disabledBg = disabled
+    ? cx !== undefined
+      ? `${cx}/50`
+      : 'bg-primary/50'
+    : bg
+  const isOutline = outline ? 'bg-transparent border border-bg-primary' : bg
+
+  return (
+    <button
+      type={type}
+      disabled={disabled}
+      className={`${isOutline} shrink-0 rounded-lg md:px-4 px-3 md:py-3 py-2  ${disabledBg} disabled:cursor-not-allowed`}
+      onClick={onClick}
+    >
+      {(typeof children === 'string') ? <Text size='sm' white={!textBlack}>
+        {children}
+      </Text> : children}
+
+    </button>
+  )
+}
