826a7bcc035e1d559353649a9b92b67e45277de5
Mmesoma Saint <mmesomasaint@gmail.com>
2024-01-09 20:04:49 +0100

    Fix: Simplify routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,4 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 import Landing from './pages/Landing'
 import SignIn from './pages/auth/signin'
 import SignUp from './pages/auth/signup'
@@ -10,45 +6,50 @@ import ForgotPassword from './pages/auth/forgotpsw'
 import UpdatePassword from './pages/auth/updatepsw'
 import Profile from './pages/profile/page'
 import EditProfile from './pages/profile/edit'
-import CreateJob from './pages/createjob'
 import Proposals from './pages/proposals/page'
-
-const routerr = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Landing />}>
-      <Route path='auth/signin' element={<SignIn />} />
-      <Route path='auth/signup' element={<SignUp />} />
-      <Route path='auth/forgotpassword' element={<ForgotPassword />} />
-      <Route path='auth/updatepassword' element={<UpdatePassword />} />
-      <Route path=':employerId/profile' element={<Profile />} />
-      <Route path=':employerId/profile/edit' element={<EditProfile />} />
-      <Route path=':employerId/createjob' element={<CreateJob />} />
-      {/** view job route */}
-      <Route path=':employerId/job/:jobId/proposals' element={<Proposals />} />
-      <Route
-        path='/:employerId/job/:jobId/proposals/proposal/:proposalId'
-        element={<Proposals />}
-      />
-    </Route>
-  )
-)
+import CreateJob from './pages/job/create'
+import Proposal from './pages/proposals/proposal'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Landing />,
   },
   {
-    path: "/auth/signin",
+    path: '/auth/signin',
     element: <SignIn />,
   },
   {
-    path: "/auth/signup",
+    path: '/auth/signup',
     element: <SignUp />,
   },
   {
-    path: "/auth/forgotpassword",
+    path: '/auth/forgotpassword',
     element: <ForgotPassword />,
+  },
+  {
+    path: '/auth/updatepassword',
+    element: <UpdatePassword />,
+  },
+  {
+    path: '/:employerId/profile',
+    element: <Profile />,
+  },
+  {
+    path: '/:employerId/profile/edit',
+    element: <EditProfile />,
+  },
+  {
+    path: '/:employerId/job/create',
+    element: <CreateJob />,
+  },
+  {
+    path: '/:employerId/job/jobId/proposals',
+    element: <Proposals />,
+  },
+  {
+    path: '/:employerId/job/jobId/proposals/proposal/:proposalId',
+    element: <Proposal />,
   },
 ])
 
