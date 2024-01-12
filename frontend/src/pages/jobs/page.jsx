ac565e09cf61745da16924d58133f796fbfcc38a
Mmesoma Saint <mmesomasaint@gmail.com>
2024-01-12 05:57:49 +0100

    Fix: Simplify jobs

--- frontend/src/pages/jobs/page.jsx
+++ frontend/src/pages/jobs/page.jsx
@@ -1,20 +1,14 @@
 import Layout from '../../components/layout'
-import Text from '../../components/text'
 import JobCard from './card'
 
 export default function Jobs() {
   return (
     <Layout>
-      <div className='flex flex-col gap-20'>
-        <div className='flex items-center'>
-          <Text size='md'>Filter</Text>
-        </div>
-        <div className='flex flex-col'>
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-        </div>
+      <div className='flex flex-col'>
+        <JobCard />
+        <JobCard />
+        <JobCard />
+        <JobCard />
       </div>
     </Layout>
   )
