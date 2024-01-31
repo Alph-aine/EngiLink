be4368d9a9a0bcc0ca653baaaa2efc192aee86fb
Abdulbasit <aabdulrafiu721@gmail.com>
2024-01-31 08:31:49 +0100

    update cookie options

--- backend/utils/sendcookie.js
+++ backend/utils/sendcookie.js
@@ -9,8 +9,7 @@ const sendToken = (user, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRATION * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: true,
-    sameSite: 'none'
+    secure: true
   };
 
   res.status(statusCode).cookie('token', token, options).json({
